6d88c42414914681c9de5114d308c282aa93e6d8
adityachemudupaty <achemudupaty@gmail.com>
2020-11-13 17:55:11 -0600

    number of arguments

--- scientific-computing-python/arithmetic_arranger.py
+++ scientific-computing-python/arithmetic_arranger.py
@@ -7,26 +7,59 @@ def arithmetic_arranger(problems):
     #If len of problems list is greater than 5, then return 'Error: Too many problems.'
     if len(problems) > 5:
         return('Error: Too many problems.')
+    
+    #How many problems?
+    problem_number = len(problems)
 
     #Change the strings received to sequence of ints and operand
-    for i in range(len(problems)):
-        problem_split = problems[i].split()
-        
-        #Operator should be '+' or '-' only
-        if problem_split[1] != '+' or problem_split[1] != '-':
-            return('Error: Operator must be "+" or "-" only.')
-        
-        #Operands should contain only digits
-        try:
-            operator1 = int(problem_split[0])
-            operator2 = int(problem_split[2])
-        except:
-            return('Error: Numbers must contain only digits.')
+    if problem_number == 1:
+        problem_split1 = problems[0].split()
+    elif problem_number == 2:
+        problem_split1 = problems[0].split()
+        problem_split2 = problems[1].split()
+    elif problem_number == 3:
+        problem_split1 = problems[0].split()
+        problem_split2 = problems[1].split()
+        problem_split3 = problems[2].split()
+    elif problem_number == 4:
+        problem_split1 = problems[0].split()
+        problem_split2 = problems[1].split()
+        problem_split3 = problems[2].split()
+        problem_split4 = problems[3].split()
+    elif problem_number == 5:
+        problem_split1 = problems[0].split()   
+        problem_split2 = problems[1].split()   
+        problem_split3 = problems[2].split()   
+        problem_split4 = problems[3].split()
+        problem_split5 = problems[4].split() 
 
-        #Operands should not be longer than 4 digits
-        if operator1 > 9999 or operator2 > 9999:
-            return('Error: Numbers cannot be more than four digits.')
+    #Operator should be '+' or '-' only
+    if problem_split[1] != '+' or problem_split[1] != '-':
+        return('Error: Operator must be "+" or "-" only.')
         
+    #Operands should contain only digits
+    try:
+        operator1 = int(problem_split[0])
+        operator2 = int(problem_split[2])
+    except:
+        return('Error: Numbers must contain only digits.')
+
+    #Operands should not be longer than 4 digits
+    if operator1 > 9999 or operator2 > 9999:
+        return('Error: Numbers cannot be more than four digits.')
+        
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
