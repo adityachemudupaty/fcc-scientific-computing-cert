0c1673c0e8579ab251775f5713b89e16c7697baf
adityachemudupaty <achemudupaty@gmail.com>
2020-11-18 14:43:43 -0600

    working on time_calculator

--- scientific-computing-python/time-calculator/time_calculator.py
+++ scientific-computing-python/time-calculator/time_calculator.py
@@ -1,7 +1,20 @@
-def add_time(start, duration):
+def add_time(start, duration, day = False):
+    
+    # Split the start time into two parts, the time and the half of the day
+    start_list= start.split()
+    start_time = start_list[0]
+
+    # Convert the time to [time in minutes since 0:00]
+    if start_list[1] == 'PM':
+        hours_to_add = 12
+    else:
+        hours_to_add = 0
+    s_hour = start_time.split(':')[0]
+    s_time = start_time.split(':')[1]
+    print(s_hour)
+    print(s_time)
+
+#    return new_time
 
 
-
-
-
-    return new_time
+add_time('3:00 PM', '3:00')
